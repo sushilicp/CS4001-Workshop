03f46199e16f98309f1bf53777929db1c47bbabd
Sushil <sushil.parajuli@veniosystems.com>
2026-01-06 16:18:24 +0545

    week01 added

---
+++ week01/WelcomeMessage.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class WelcomeMessage here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WelcomeMessage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to programming.");
+        System.out.println("My name is Sushil Parajuli.");
+        
+    }
+}
