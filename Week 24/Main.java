4c6031cf16196c34618648bb51ca8227d7f4983d
sushilicp <sushil.parajuli@icp.edu.np>
2026-04-15 12:55:07 +0545

    Update Main.java

--- Week 24/Main.java
+++ Week 24/Main.java
@@ -1,29 +1,20 @@
-import java.io.*;
+import java.io.Serializable;
 
-public class Main {
-    public static void main(String[] args) {
-        readFromFile();
+public class Teacher implements Serializable {
+    long salary;
+    int age;
+    String name;
+
+    float rating;
+
+    Teacher(String name,long salary,int age,float rating){
+        this.name=name;
+        this.salary=salary;
+        this.age=age;
+        this.rating=rating;
     }
 
-    private static void writeToFile() {
-        Teacher teacher = new Teacher("Sushil",1000L,45,7.1f);
-
-        try(ObjectOutputStream os=new ObjectOutputStream(new FileOutputStream("teacher.dat")))
-        {
-            os.writeObject(teacher);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private static void readFromFile() {
-       Teacher teacher;
-
-        try(ObjectInputStream os=new ObjectInputStream(new FileInputStream("teacher.dat")))
-        {
-            teacher = (Teacher)os.readObject();
-            System.out.println(teacher.name+" "+teacher.age+" "+teacher.rating);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String toString(){
+        return "Name:"+this.name+" Salary:"+this.salary+" Age:"+this.age+" Rating:"+this.rating;
     }
 }
