21fefc3ca88b3a989e826ae3ed33170a511e6fa8
sushilicp <sushil.parajuli@icp.edu.np>
2026-04-15 12:55:45 +0545

    Update Main.java

--- Week 24/Main.java
+++ Week 24/Main.java
@@ -1,20 +1,41 @@
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
 
-public class Teacher implements Serializable {
-    long salary;
-    int age;
-    String name;
-
-    float rating;
-
-    Teacher(String name,long salary,int age,float rating){
-        this.name=name;
-        this.salary=salary;
-        this.age=age;
-        this.rating=rating;
+public class Main {
+    public static void main(String[] args) {
+        writeToFile();
+        readFromFile();
     }
 
-    public String toString(){
-        return "Name:"+this.name+" Salary:"+this.salary+" Age:"+this.age+" Rating:"+this.rating;
+    private static void writeToFile() {
+        Teacher teacher = new Teacher("Sushil",1000L,45,7.1f);
+        Teacher teacher1 = new Teacher("Sushil1",1000L,45,7.1f);
+        Teacher teacher2 = new Teacher("Sushil2",1000L,45,7.1f);
+        Teacher teacher3 = new Teacher("Sushil3",1000L,45,7.1f);
+        ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+        teachers.add(teacher);
+        teachers.add(teacher1);
+        teachers.add(teacher2);
+        teachers.add(teacher3);
+
+        try(ObjectOutputStream os=new ObjectOutputStream(new FileOutputStream("teacher.dat")))
+        {
+            os.writeObject(teachers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void readFromFile() {
+        ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+
+        try(ObjectInputStream os=new ObjectInputStream(new FileInputStream("teacher.dat")))
+        {
+            teachers = (ArrayList<Teacher>) os.readObject();
+           for(Teacher t:teachers){
+               System.out.println(t);
+           }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
